5e35c942f7d42f11d335b746a222b2db3e6c8c67
Amanda Vanderheyden <aev4882@gmail.com>
2019-10-13 12:43:06 -0500

    take-off-button-message

--- scripts.js
+++ scripts.js
@@ -1,9 +1,13 @@
 // Write your JavaScript code here.
 window.addEventListener('load', function() {
     console.log("Page is loaded");
+//change flight status to "shutttle is in flight"//
 let flightStatus= document.getElementById("flightStatus");
 document.getElementById("flightStatus").innerHTML= "Shuttle in flight!";
 console.log(flightStatus.innerHTML);
+//window: "Confirm that the shuttle is ready for takeoff."//
+document.getElementById("takeoff").addEventListener("click",function(){alert("Confirm that the shuttle is ready for takeoff."); });
+
 // Remember to pay attention to page loading!
 });
 // function handleClick("Ready") {

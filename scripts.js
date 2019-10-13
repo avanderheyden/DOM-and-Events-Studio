b4b743c93f81a40fbe4e7fa369611e49f46309a6
Amanda Vanderheyden <aev4882@gmail.com>
2019-10-13 12:34:26 -0500

    flight-status-changed

--- scripts.js
+++ scripts.js
@@ -1,8 +1,19 @@
 // Write your JavaScript code here.
-function handleClick() {
-    alert("Confirm that the shuttle is ready for liftoff");
-}
-function flightStatus() {
-    flightStatus ="Shuttle in flight";
-}
+window.addEventListener('load', function() {
+    console.log("Page is loaded");
+let flightStatus= document.getElementById("flightStatus");
+document.getElementById("flightStatus").innerHTML= "Shuttle in flight!";
+console.log(flightStatus.innerHTML);
 // Remember to pay attention to page loading!
+});
+// function handleClick("Ready") {
+//     alert("Confirm that the shuttle is ready for liftoff");
+// }
+// function handleClick("color") {
+//     element.style="background-color: blue;"
+//     ;
+// }
+
+// // onclick="handleClick('color')"
+// // onclick = "handleClick('ready')"
+// <!-- window.addEventListener("click", function handleClick(){}) -->

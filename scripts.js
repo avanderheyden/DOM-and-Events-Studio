b8ff8d8f20b3f20f1f059a98010f01d922f30dd1
Amanda Vanderheyden <aev4882@gmail.com>
2019-10-13 13:35:10 -0500

    shuttle-height-changed

--- scripts.js
+++ scripts.js
@@ -2,16 +2,19 @@
 window.addEventListener('load', function() {
     console.log("Page is loaded");
 //change flight status to "shutttle is in flight"//
-let flightStatus= document.getElementById("flightStatus");
-document.getElementById("flightStatus").innerHTML= "Shuttle in flight!";
-console.log(flightStatus.innerHTML);
+    let flightStatus= document.getElementById("flightStatus");
+    document.getElementById("flightStatus").innerHTML= "Shuttle in flight!";
+        console.log(flightStatus.innerHTML);
 //window: "Confirm that the shuttle is ready for takeoff."//
-let takeOffButton= document.getElementById("takeoff");
-document.getElementById("takeoff").addEventListener("click",function(){alert("Confirm that the shuttle is ready for takeoff."); });
+    let takeOffButton= document.getElementById("takeoff");
+    document.getElementById("takeoff").addEventListener("click",function(){alert("Confirm that the shuttle is ready for takeoff."); });
 //change background color from green to blue//
-let backgroundColor= document.querySelector("#shuttleBackground");
-document.querySelector("#shuttleBackground").style.background= "blue";
+    let backgroundColor= document.querySelector("#shuttleBackground");
+    document.querySelector("#shuttleBackground").style.background= "blue";
     console.log(backgroundColor);
+//shuttle height increases by 10,000//
+    let shuttleHeight=
+    document.getElementById('spaceShuttleHeight').innerHTML="10,000";
 // Remember to pay attention to page loading!
 });
 // function handleClick("Ready") {

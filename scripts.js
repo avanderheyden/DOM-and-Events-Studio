e0ce041f325e8b0a90b32ec3b0f837a88be595cc
Amanda Vanderheyden <aev4882@gmail.com>
2019-10-13 11:59:46 -0500

    window-load-event

--- scripts.js
+++ scripts.js
@@ -1,2 +1,8 @@
 // Write your JavaScript code here.
+function handleClick() {
+    alert("Confirm that the shuttle is ready for liftoff");
+}
+function flightStatus() {
+    flightStatus ="Shuttle in flight";
+}
 // Remember to pay attention to page loading!
